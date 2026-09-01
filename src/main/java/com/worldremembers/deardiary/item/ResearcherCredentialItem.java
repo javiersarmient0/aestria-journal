39625530bf5ca92eb33b86977a11de32c52d0f23
javiersarmient0 <javierver37@gmail.com>
2026-09-01 01:57:08 -0300

    Add researcher credential item for initial GUI test

---
+++ src/main/java/com/worldremembers/deardiary/item/ResearcherCredentialItem.java
@@ -0,0 +1,24 @@
+package com.worldremembers.deardiary.item;
+
+import com.worldremembers.deardiary.network.DearDiaryNetworking;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+public final class ResearcherCredentialItem extends Item {
+    public ResearcherCredentialItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+        if (!world.isClient && user instanceof net.minecraft.server.network.ServerPlayerEntity serverPlayer) {
+            DearDiaryNetworking.openResearcherCredential(serverPlayer);
+        }
+        return ActionResult.SUCCESS;
+    }
+}
